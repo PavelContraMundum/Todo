847bc979b6af175b9240623eabc631b7daa7e582
PavelContraMundum <pavel.sticka@gmail.com>
2024-11-05 20:57:24 +0100

    přidáno mazání úkolů

--- todo.c
+++ todo.c
@@ -226,6 +226,44 @@ void markCompleted()
     saveTodos(); // Automatické uložení po změně
 }
 
+void deleteTodo()
+{
+    if (todoCount == 0)
+    {
+        printf("Seznam je prázdný!\n");
+        return;
+    }
+
+    listTodos();
+
+    int index;
+    printf("Zadejte číslo úkolu ke smazání: ");
+    if (scanf("%d", &index) != 1)
+    {
+        printf("Neplatný vstup!\n");
+        clearInputBuffer();
+        return;
+    }
+
+    if (index < 1 || index > todoCount)
+    {
+        printf("Neplatné číslo úkolu!\n");
+        return;
+    }
+
+    // Přesun všech ostatních úkolů o jeden index dolů
+    for (int i = index - 1; i < todoCount - 1; i++)
+    {
+        strcpy(todos[i].description, todos[i + 1].description);
+        todos[i].completed = todos[i + 1].completed;
+        todos[i].priority = todos[i + 1].priority;
+    }
+
+    todoCount--;
+    printf("Úkol byl smazán.\n");
+    saveTodos();
+}
+
 // Hlavní menu
 void showMenu()
 {
@@ -233,7 +271,8 @@ void showMenu()
     printf("1. Přidat úkol\n");
     printf("2. Zobrazit úkoly\n");
     printf("3. Označit úkol jako splněný\n");
-    printf("4. Konec\n");
+    printf("4. Smazat úkol\n");
+    printf("5. Konec\n");
     printf("Vyberte možnost: ");
 }
 
@@ -266,12 +305,15 @@ int main()
             markCompleted();
             break;
         case 4:
+            deleteTodo();
+            break;
+        case 5:
             printf("Nashledanou!\n");
             break;
         default:
             printf("Neplatná volba!\n");
         }
-    } while (choice != 4);
+    } while (choice != 5);
 
     return 0;
 }
